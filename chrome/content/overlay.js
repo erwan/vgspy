f1843eaa1e7bf450879762259c0b859d24174050
Erwan Loisant <eloisant@gmail.com>
2008-07-04 22:16:35 -0700

    panel, licences

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,18 +1,32 @@
+/*
+ * Copyright 2008 Erwan Loisant
+ *
+ * This file may be used under the terms of of the
+ * GNU General Public License Version 2 or later (the "GPL"),
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ */
+
 var vgspy = {
   onLoad: function() {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("vgspy-strings");
   },
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
-  },
   onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    vgspy.onMenuItemCommand(e);
+    var panel = document.getElementById("vgspyPanel");
+    panel.openPopup(document.getElementById("vgspy-toolbar-button"),
+                    "after_start",
+                    100,
+                    0,
+                    false,
+                    false);
+    panel.focus();
   }
 
 };
