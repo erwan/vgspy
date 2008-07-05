8e613dcd6110a3e0ea989870c4314b341cf26f6c
Erwan Loisant <eloisant@gmail.com>
2008-07-05 11:37:16 -0700

    Basic Amazon functionalities

---
+++ chrome/content/amazon.js
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2008 Erwan Loisant
+ *
+ * This file may be used under the terms of of the
+ * GNU General Public License Version 2 or later (the "GPL"),
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Portion of this code from The Sage Project http://sage.mozdev.org
+ *
+ */
+
+function vgsAmazonLoader(aListener) {
+  this.listener = aListener;
+}
+
+vgsAmazonLoader.prototype = {
+  ACCESSID: "0ABF9S44F25FA1XJ22G2",
+
+  RESULT_OK: 0,
+  RESULT_PARSE_ERROR: 1,
+  RESULT_NOT_RSS: 2,
+  RESULT_NOT_FOUND: 3,
+  RESULT_NOT_AVAILABLE: 4,
+  RESULT_ERROR_FAILURE: 5,
+
+  abort: function() {
+    if (this.loading) {
+      if (this.httpReq) {
+        this.httpReq.abort();
+      }
+      this.httpReq = null;
+      this.loading = false;
+      this._callListeners("abort", this.uri);
+    }
+  },
+
+  onHttpError: function(e) {
+    this.httpGetResult(this.RESULT_NOT_AVAILABLE);
+  },
+
+  onHttpReadyStateChange: function(e) {
+    if (this.httpReq.readyState == 2)
+    {
+      try
+      {
+        if (this.httpReq.status == 404)
+        {
+          this.httpGetResult(this.RESULT_NOT_FOUND);
+        }
+      }
+      catch (e)
+      {
+        this.httpGetResult(this.RESULT_NOT_AVAILABLE);
+        return;
+      }
+    }
+  },
+
+  onHttpLoaded: function(e) {
+    var title;
+    var cover;
+    var manufacturer;
+    var price = null;
+    var lowestprice = null;
+    var usedprice = null;
+    var url;
+
+    var xml = this.httpReq.responseXML;
+    var itemSearchResponse = xml.getElementsByTagName("ItemSearchResponse")[0];
+    var items = itemSearchResponse.getElementsByTagName("Items")[0];
+    var item = items.getElementsByTagName("Item")[0];
+    var image = item.getElementsByTagName("MediumImage")[0];
+    cover = image.getElementsByTagName("URL")[0].firstChild.nodeValue;
+    var attributes = item.getElementsByTagName("ItemAttributes")[0];
+    title = attributes.getElementsByTagName("Title")[0].firstChild.nodeValue;
+    url = item.getElementsByTagName("DetailPageURL")[0].firstChild.nodeValue;
+    manufacturer = attributes.getElementsByTagName("Manufacturer")[0].firstChild.nodeValue;
+    price = attributes.getElementsByTagName("ListPrice")[0]
+                      .getElementsByTagName("FormattedPrice")[0].firstChild.nodeValue;
+    var offer = item.getElementsByTagName("OfferSummary")[0];
+    if (offer.getElementsByTagName("LowestNewPrice") &&
+        offer.getElementsByTagName("LowestNewPrice")[0]) {
+      lowestprice = offer.getElementsByTagName("LowestNewPrice")[0]
+                   .getElementsByTagName("FormattedPrice")[0].firstChild.nodeValue;
+    }
+    if (offer.getElementsByTagName("LowestUsedPrice") &&
+        offer.getElementsByTagName("LowestUsedPrice")[0]) {
+      usedprice = offer.getElementsByTagName("LowestUsedPrice")[0]
+                       .getElementsByTagName("FormattedPrice")[0].firstChild.nodeValue;
+    }
+
+    this.currentItem = {
+      title: title,
+      cover: cover,
+      manufacturer: manufacturer,
+      price: price,
+      lowestprice: lowestprice,
+      usedprice: usedprice,
+      url: url
+    };
+    this.listener.onSuccess("load", this.currentItem);
+    this.httpReq = null;
+  },
+
+  httpGetResult: function(aResultCode) {
+    this.loading = false;
+    this.listener.onError("error", aResultCode);
+    // clean up
+    this.httpReq = null;
+  },
+
+  _makeURL: function(aTitle) {
+    var base = "http://ecs.amazonaws.com/onca/xml"
+    var args = {
+      Service: "AWSECommerceService",
+      AWSAccessKeyId: this.ACCESSID,
+      Operation: "ItemSearch",
+      SearchIndex: "VideoGames",
+      ResponseGroup: "Large,Images,OfferSummary",
+      Title: encodeURIComponent(aTitle)
+    }
+
+    var result = base;
+    for (let key in args) {
+      result += (result == base) ? "?" : "&";
+      result += (key + "=" + args[key]);
+    }
+
+    return result;
+  },
+
+  query: function(aTitle) {
+    this.httpReq = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
+                   .createInstance(Components.interfaces.nsIXMLHttpRequest);
+    this.httpReq.mozBackgroundRequest = true;
+    this.httpReq.open("GET", this._makeURL(aTitle));
+
+    var oThis = this;
+    this.httpReq.onload = function (e) { return oThis.onHttpLoaded(e); };
+    this.httpReq.onerror = function (e) { return oThis.onHttpError(e); };
+    this.httpReq.onreadystatechange = function (e) { return oThis.onHttpReadyStateChange(e); };
+
+    try {
+      this.httpReq.send(null);
+    } catch (ex) {
+    }
+  }
+}
--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -14,20 +14,77 @@
 
 var vgspy = {
   onLoad: function() {
-    // initialization code
     this.initialized = true;
     this.strings = document.getElementById("vgspy-strings");
-  },
-  onToolbarButtonCommand: function(e) {
-    var panel = document.getElementById("vgspyPanel");
-    panel.openPopup(document.getElementById("vgspy-toolbar-button"),
-                    "after_start",
-                    0,
-                    0,
-                    false,
-                    false);
-    panel.focus();
-  }
 
+    this.panel = document.getElementById("vgspyPanel");
+
+    this.cover = document.getElementById("vgspyPanelCover");
+    this.title = document.getElementById("vgspyPanelTitle");
+    this.subtitle = document.getElementById("vgspyPanelSubtitle");
+    this.pricesBox = document.getElementById("vgspyPrices");
+  },
+
+  _clearPrices: function() {
+    while (this.pricesBox.firstChild) {
+      this.pricesBox.removeChild(this.pricesBox.firstChild);
+    }
+  },
+
+  _addPrice: function(aLabel, aPrice, aURL) {
+    var price = document.createElement("hbox");
+    var inst = this;
+    price.onclick = function(event) {
+      inst.panel.hidePopup();
+      openUILinkIn(aURL, "tab");
+    }
+    var priceLabel = document.createElement("description");
+    priceLabel.setAttribute("value", aLabel);
+    var space = document.createElement("spacer");
+    space.setAttribute("flex", "1");
+    var priceValue = document.createElement("description");
+    priceValue.setAttribute("value", aPrice);
+
+    price.appendChild(priceLabel);
+    price.appendChild(space);
+    price.appendChild(priceValue);
+
+    this.pricesBox.appendChild(price);
+  },
+
+  showPanel: function(e) {
+    this._clearPrices();
+    this.panel.openPopup(document.getElementById("vgspy-toolbar-button"),
+                         "after_start",
+                         0,
+                         0,
+                         false,
+                         false);
+    this.panel.focus();
+
+    var inst = this;
+    var listener = {
+      onSuccess: function(aSubject, aResult) {
+        inst.cover.setAttribute("src", aResult.cover);
+        inst.title.setAttribute("value", aResult.title);
+        inst.subtitle.setAttribute("value", aResult.manufacturer);
+        if (aResult.price !== null) {
+          inst._addPrice("Amazon", aResult.price, aResult.url);
+        }
+        if (aResult.price !== null) {
+          inst._addPrice("Amazon (lowest new)", aResult.lowestprice, aResult.url);
+        }
+        if (aResult.usedprice !== null) {
+          inst._addPrice("Amazon (used)", aResult.usedprice, aResult.url);
+        }
+      },
+      onError: function(aSubject, aCode) {
+      }
+    }
+
+    var loader = new vgsAmazonLoader(listener);
+    loader.query("Super Mario Galaxy");
+  }
 };
+
 window.addEventListener("load", function(e) { vgspy.onLoad(e); }, false);
