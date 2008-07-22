0035ec18cc18fe36abccb3269825a6247c47d8e6
Erwan Loisant <eloisant@gmail.com>
2008-07-22 01:34:11 -0700

    add common.js

---
+++ chrome/content/common.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2008 Erwan Loisant
+ *
+ * This file may be used under the terms of of the
+ * GNU General Public License Version 2 or later (the "GPL"),
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ */
+
+const CC = Components.classes;
+const CI = Components.interfaces;
+
+var VGSpyCommon = {
+  CONDITION_PREMIUM: 1,
+  CONDITION_NEW: 2,
+  CONDITION_USED: 3
+}
